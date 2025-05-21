0d3a36f2100677bea2747380910913c9e64e2d93
subhamse <129133175+subhamse@users.noreply.github.com>
2025-05-21 17:46:32 +0530

    Update main.tf

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 data "aws_ami" "ubuntu" {
@@ -25,18 +25,6 @@ data "aws_ami" "ubuntu" {
 locals {
   instances = {
     instance1 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
-    instance2 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
-    instance3 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
-    instance4 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"
     }
